eaca5821807938140210c38190a5f0e4a3fb79da
alexander-n8hgeg5e <alexander.w.email@gmail.com>
2019-03-24 16:01:24 +0100

    update: new stuff to github

--- profile_default/ipython_config.py
+++ profile_default/ipython_config.py
@@ -7,6 +7,20 @@ c.IPCompleter.jedi_compute_type_timeout=10000
 c.TerminalInteractiveShell.autocall=0
 #c.TerminalInteractiveShell.highlighting_style='native'
 #c.TerminalInteractiveShell.colors='neutral'
+c.TerminalInteractiveShell.color_info=False
 c.TerminalInteractiveShell.editing_mode = 'vi'
 c.TerminalInteractiveShell.space_for_menu=20
 c.HistoryManager.db_log_output=True
+
+
+f=open( "/tmp/background", mode='wt+' )
+data=f.readline().strip()
+if data == 'light':
+    c.TerminalInteractiveShell.colors='LightBG'
+elif data == 'dark':
+    c.TerminalInteractiveShell.colors='Neutral'
+else:
+    c.TerminalInteractiveShell.colors='Linux'
+    f.seek(0);
+    f.writelines("Neutral")
+    f.close()
